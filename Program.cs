3ded6e4246bca132c2a9b7aecd947d8d37867973
Florian Bennecker <f.bennecker@bennecker.eu>
2023-08-26 10:57:40 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+namespace EncryptionConfig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EncryptString(true);
+            ConnectionStringSettings connectionString = ConfigurationManager.ConnectionStrings["ConnectionString01"];
+            Console.WriteLine(connectionString);
+        }
+
+        public static void EncryptString(bool encrypt)
+        {
+            Configuration? config = null;
+            
+            try
+            {
+                // open app.config and retrieve connectionStrings-section
+                config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                ConnectionStringsSection connectionStringsSection = config.GetSection("connectionStrings") as ConnectionStringsSection;
+
+                // if - section & element are not locked
+                if ((!connectionStringsSection.ElementInformation.IsLocked) && (!connectionStringsSection.SectionInformation.IsLocked)) 
+                {
+                    // if - enrypt the section
+                    if (encrypt && !connectionStringsSection.SectionInformation.IsProtected)
+                    {
+                        connectionStringsSection.SectionInformation.ProtectSection("DataProtectionConfigurationProvider");
+                    }
+
+                    // if - enrypt is false --> decrypt
+                    if (!encrypt && connectionStringsSection.SectionInformation.IsProtected)
+                    {
+                        connectionStringsSection.SectionInformation.UnprotectSection();
+                    }
+
+                    // save the new app.config and open in notepad
+                    connectionStringsSection.SectionInformation.ForceSave = true;
+                    config.Save();
+
+                    Process.Start("notepad.exe", config.FilePath);
+                    
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
